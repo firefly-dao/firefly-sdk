59abc041d503f4c38f1c1b04ec191dbdf3221f7a
jerome <1031191411@qq.com>
2026-01-07 10:52:06 +0800

    chore: remove unused files

--- src/api/chain.ts
+++
@@ -1,6 +0,0 @@
-import { TESTNET_FIREFLY_API, MAINNET_FIREFLY_API } from "../constants/index.js";
-import axios from "axios";
-export function getChainsApi(isTestnet: boolean = false) {
-  const baseApiUrl = isTestnet ? TESTNET_FIREFLY_API : MAINNET_FIREFLY_API
-  return axios.get(`${baseApiUrl}/quote/chains`)
-}
